908e6540e25083eb50e7d02af4b848406ad43c7b
Guilherme Popolin <guilhermespopolin@gmail.com>
2018-07-22 15:34:31 -0300

    cfg(jest): adding md files on test match to use file mock

--- jest.config.js
+++ jest.config.js
@@ -22,9 +22,10 @@ module.exports = {
     'json',
   ],
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|md)$':
+      '<rootDir>/src/__mocks__/fileMock.js',
     '\\.(css|less)$': 'identity-obj-proxy',
   },
-  modulePaths: ['<rootDir>/src'],
+  modulePaths: ['<rootDir>', '<rootDir>/src'],
   setupFiles: ['./jestsetup.config.js'],
 };
