95a8682e36fa4cba60fdec4c5184f996ee6cd51f
Guilherme Popolin <guilhermespopolin@gmail.com>
2018-08-06 20:19:38 -0300

    feat: adding db generation function for json-server

---
+++ src/__mocks__/db.js
@@ -0,0 +1,22 @@
+const faker = require('faker');
+
+function generateEmployees(qtd = 50) {
+  const employees = [];
+
+  for (let id = 0; id < qtd; id += 1) {
+    const firstName = faker.name.firstName();
+    const lastName = faker.name.lastName();
+    const email = faker.internet.email();
+
+    employees.push({
+      id,
+      firstName,
+      lastName,
+      email,
+    });
+  }
+
+  return { employees };
+}
+
+module.exports = generateEmployees;
