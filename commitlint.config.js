052e93b181232afc3c07afff83c1769869f37c28
Guilherme Popolin <guilhermespopolin@gmail.com>
2018-08-12 16:06:23 -0300

    chore: adding @commitlint/prompt-cli and updating @commintlint config
    In order to use @commilint default configuration, previous customized configuration added was removed. Additionally, a cli tool was added (@commitlint-promp-cli) in order to help authoring commit messages that follows an specific pattern.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,3 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', 'cfg', 'chore'],
-    ],
-  },
 };
