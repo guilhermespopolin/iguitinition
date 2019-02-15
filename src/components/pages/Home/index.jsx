53bf46ddfca92eaaca562de0bce4c6a211261b96
Guilherme Popolin <guilhermespopolin@gmail.com>
2019-02-15 01:11:49 -0200

    refactor(home): cleaner version

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -1,41 +1,34 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import Text from 'components/ui/Text'
 import Image from 'components/ui/Image'
 
 import reactLogo from 'assets/imgs/react_logo.svg'
 
+const StyledHome = styled.main`
+  height: 100vh;
+`
+
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 40%;
+  background-color: ${({ theme }) => theme.colors.black};
+  color: ${({ theme }) => theme.colors.white};
+`
+
 function Home() {
   return (
-    <main css="height: 100vh;">
-      <header
-        css={`
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          height: 40%;
-          background-color: ${({ theme }) => theme.colors.black};
-          color: ${({ theme }) => theme.colors.white};
-        `}
-      >
-        <Image
-          css={`
-            & {
-              transition: all 0.5s ease;
-            }
-
-            &:hover {
-              transform: scale(1.1);
-            }
-          `}
-          src={reactLogo}
-          size="large"
-        />
-      </header>
-      <Text css="padding: 0 0.5rem;" alignment="center">
+    <StyledHome>
+      <Header>
+        <Image src={reactLogo} size="large" />
+      </Header>
+      <Text alignment="center">
         To get started, edit <code>src/components/pages/Home/index.jsx</code> and save to reload
       </Text>
-    </main>
+    </StyledHome>
   )
 }
 
