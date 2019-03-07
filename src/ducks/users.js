390df695839952c79f8504c5ba785a67d5844fff
Guilherme Popolin <guilhermespopolin@gmail.com>
2019-03-06 22:19:02 -0300

    refactor(users): updating ducks

--- src/ducks/users.js
+++ src/ducks/users.js
@@ -3,6 +3,11 @@ import {
   updateResource,
   removeResource,
 } from 'ducks/resources'
+import {
+  loading,
+  fail,
+  finish,
+} from 'ducks/communication'
 
 // Action Creators
 export function setUsers(payload) {
@@ -20,14 +25,17 @@ export function removeUser(userId) {
 // Side Effects
 export function fetchUsers() {
   return async (dispatch) => {
+    // you need to run mock-server script in order this to work
     const url = 'http://localhost:3042/users'
+    dispatch(loading('USERS'))
     try {
       const response = await fetch(url)
       const payload = await response.json()
 
       dispatch(setUsers(payload))
+      dispatch(finish('USERS'))
     } catch (e) {
-      throw new Error(e)
+      fail('USERS', e)
     }
   }
 }
