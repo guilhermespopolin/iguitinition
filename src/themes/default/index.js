21bfea75fc03a87407c4dc8f247f18b56df9dabe
Guilherme Popolin <guilhermespopolin@gmail.com>
2019-07-20 00:45:15 -0300

    style: update theme colors

--- src/themes/default/index.js
+++ src/themes/default/index.js
@@ -1,12 +1,21 @@
 export default {
   colors: {
-    white: '#ffffff',
-    black: '#000000',
-    lightGray: '#e3e3e3',
-    darkGray: '#646464',
-    primary: '#4030e8',
-    primaryLight: '#4030ff',
-    secondary: '#ff9900',
+    primary: '#0a30ad',
+    primary1: '#3e60d2',
+    primary2: '#1542d4',
+    primary3: '#07268b',
+    primary4: '#041c6b',
+
+    complement: '#ffb000',
+    complement1: '#ffc034',
+    complement2: '#ffb000',
+    complement3: '#cd8d00',
+    complement4: '#9e6d00',
+
+    white: '#fefefe',
+    black: '#020408',
+    darkGray: '#737682',
+    lightGray: '#e6e9f3',
   },
   fontSize: {
     small: '0.9rem',
